09941760e8415eb582566dc0a0d37eb726a3d950
M0Rph3U56031769 <nagydaniel1337@gmail.com>
2017-03-22 15:17:15 +0100

    translate the config value

---
+++ unittest_for_the_module.py
@@ -0,0 +1,23 @@
+# unittesting the configParzolo.py
+
+import unittest
+
+from configParzolo import configHandler
+
+
+class TestConfigParsolo(unittest.TestCase):
+    def test_modify(self):
+        config2 = configHandler.ConfigHandler('config.cfg')
+        config2.config_modify('base', 'first', 'unmodified')
+        self.assertEqual('unmodified', config2.config_check('base', 'first'))
+
+        config2.config_modify('base', 'first', 'modified')
+        self.assertEqual('modified', config2.config_check('base', 'first'))
+
+    def test_check(self):
+        config = configHandler.ConfigHandler('config2.cfg')
+        self.assertIsNotNone(config)
+        self.assertEqual(config.config_check('base', 'first'), 'modified')
+
+if __name__ == "__main__":
+    unittest.main()
